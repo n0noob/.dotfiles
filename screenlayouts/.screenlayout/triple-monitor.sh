fb5c0b9c24b4732cd13f838a0c914ce2383560f2
n0noob <neemanoop@gmail.com>
2023-09-26 00:43:17 +0530

    first commit

---
+++ screenlayouts/.screenlayout/triple-monitor.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+xrandr  --output eDP --mode 1920x1080 --pos 3840x0 --rotate normal \
+        --output HDMI-A-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+        --output DP-1-0 --mode 1920x1080 --pos 1920x0 --rotate normal \
+        --output DP-1-1 --off
